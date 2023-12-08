52ef30f49efd57af8f8966c2f622b0a4222cc95f
Deva Praveen K <115524697+devapraveenk@users.noreply.github.com>
2023-12-09 00:23:18 +0530

    First Commit

---
+++ Base.py
@@ -0,0 +1,37 @@
+from langchain.llms import GooglePalm
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+db_file_path='FAISS_Index'
+embeddings = HuggingFaceEmbeddings()
+
+def creation_of_vectorDB_in_local(loader):
+    data = loader.load()
+    db =FAISS.from_documents(data, embeddings)
+    db.save_local(db_file_path)
+
+def creation_FAQ_chain():
+    db= FAISS.load_local(db_file_path, embeddings)
+    retriever =db.as_retriever(score_threshold=0.7)
+    
+    llm=GooglePalm(google_api_key=os.environ['GOOGLE_API_KEY'],temperature=0.0)
+    
+    prompt_temp="""Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "This Question not Present in My Database." Don't try to make up an answer.
+    CONTEXT: {context}
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(template=prompt_temp, input_variables=["context", "question"])
+    chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", 
+                                        retriever=retriever, 
+                                        input_key="query", 
+                                        return_source_documents=False,
+                                        chain_type_kwargs={"prompt" : PROMPT})
+    return chain
+
---
+++ app.py
@@ -0,0 +1,60 @@
+from langchain.document_loaders import CSVLoader
+import tempfile
+import streamlit as st
+from Base import creation_FAQ_chain,creation_of_vectorDB_in_local
+
+def csv_loader(tmp_file_path):
+    loader=CSVLoader(file_path=tmp_file_path)
+
+    return loader
+
+
+def main():
+    st.set_page_config(page_title="FAQ Chatbot",page_icon="😈",layout="wide")
+    st.title("FAQ ChatBot with your CSV file 📃")
+
+    with st.sidebar:
+        st.title("Settings")
+        st.markdown('---')
+        st.subheader('Upload Your CSV File')
+        doc=st.file_uploader("Upload your CSV file and Click Process",'csv')
+
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                if doc is not None:
+                    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                        tmp_file.write(doc.getvalue())
+                        tmp_file_path = tmp_file.name
+            
+                        st.success(f'File {doc.name} is successfully saved!')
+                    
+                    load=csv_loader(tmp_file_path)
+                    creation_of_vectorDB_in_local(load)
+                    st.success("Process Done")
+                else:
+                    st.error("❗️Please Upload Your File❗️")
+        
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    for message in st.session_state.messages:
+        with st. chat_message(message["role"]):
+            st. markdown (message["content"])
+
+    query=st.chat_input("Ask the Question")
+    if query:
+        ans=creation_FAQ_chain()
+        result=ans(query)
+        a=result["result"]
+        st. chat_message ("user"). markdown(query)
+        st. session_state. messages.append({"role": "user","content": query})
+
+        with st.chat_message("assistant"):
+            st. markdown (a)
+            st. session_state.messages. append(
+                {"role": "assistant",
+                "content": a})
+       
+
+
+if __name__=='__main__':
+    main()                
